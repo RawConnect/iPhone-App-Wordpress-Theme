7c8c29d1bf9dbae2e79b08ed810dae0cd13782ae
Dominik Krejcik <dominik.krejcik@klikni.cz>
2011-09-22 18:52:11 +0100

    Fix Twitter button and price tag positioning

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ get_header();
 					<div><span>Version:</span> <?php echo get_option('rt_appversion'); ?>  </div>     
 					<div><span>Author:</span> <?php echo get_option('rt_authorname'); ?>     </div>    
 					<div><span>Price:</span> <?php echo get_option('rt_appprice'); ?> </div> 
-					<div class="app-price-tag"><span class="pricetag-text"><?php echo get_option('rt_appprice'); ?></span></div>
-					<div class="app-price-tag app-price-tag-cover"><p> </p></div>
+					<div class="app-price-tag-wrapper"><div class="app-price-tag"><span class="pricetag-text"><?php echo get_option('rt_appprice'); ?></span></div>
+					<div class="app-price-tag app-price-tag-cover"><p> </p></div></div>
 
 				</div>
 				<div class="appstore">
@@ -29,7 +29,7 @@ get_header();
 				</div>
 				<div id="social">
 				<!-- Twitter -->
-					<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="dominikkrejcik">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+					<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-related="dominikkrejcik">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 				<!-- Facebook -->
 					<iframe src="http://www.facebook.com/plugins/like.php?app_id=142549539162044&amp;href=<?php echo rawurlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px; position: relative; top: 1px;" allowTransparency="true"></iframe>
 				<!-- G+ -->
